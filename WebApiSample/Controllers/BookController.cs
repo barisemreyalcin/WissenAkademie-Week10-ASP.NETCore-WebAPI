2cdce3ea2d129411d320dfa2ffbf51e4e7de524c
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-10-28 11:41:02 +0300

    initial commit

---
+++ WebApiSample/Controllers/BookController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSample.Model;
+
+namespace WebApiSample.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class BookController : ControllerBase
+	{
+		private List<Book> BookList = new List<Book>()
+		{
+			new Book
+			{
+				BookId = 1,
+				Title = "Suç ve Ceza",
+				PageCount = 800,
+				PublishDate = new DateTime(1994,2,12),
+				ISBN = Guid.NewGuid().ToString()
+			},
+
+			new Book
+			{
+				BookId = 2,
+				Title = "Savaş ve Barış",
+				PageCount = 800,
+				PublishDate = new DateTime(1996,6,21),
+				ISBN = Guid.NewGuid().ToString()
+			},
+
+			new Book
+			{
+				BookId = 3,
+				Title = "Yer Altından Notlar",
+				PageCount = 600,
+				PublishDate = new DateTime(2001,5,10),
+				ISBN = Guid.NewGuid().ToString()
+			},
+
+			new Book
+			{
+				BookId = 4,
+				Title = "Dönüşüm",
+				PageCount = 750,
+				PublishDate = new DateTime(1998,11,25),
+				ISBN = Guid.NewGuid().ToString()
+			},
+		};
+
+		// api/Book/Books
+		[ActionName("BookList")]
+		[HttpGet]
+		public List<Book> Books()
+		{
+			var bookList = BookList.OrderBy(x => x.PublishDate).ToList();
+			return bookList;
+		}
+
+		// İlla bir tane Get'im olmak zorunda değil
+		// api/Book/GetBookById/BookId
+		[HttpGet("{BookId}")] // beklediği parametreyi yazdık
+		public Book GetBookById(int BookId)
+		{
+			var book = BookList.SingleOrDefault(x => x.BookId == BookId);
+			return book;
+		}
+
+		[HttpGet("{BookId}")]
+		[ActionName("GetBookByIdStr")] // Farklı imzayla aynı isimli farklı method
+		public Book GetBookById(string BookId)
+		{
+			var book = BookList.SingleOrDefault(x => x.BookId == int.Parse(BookId.ToString()));
+			return book;
+		}
+	}
+}
---
+++ WebApiSample/Model/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiSample.Model
+{
+	public class Book
+	{
+		public int BookId { get; set; }
+		public string Title { get; set; }
+		public int PageCount { get; set; }
+		public DateTime PublishDate { get; set; }
+		public string ISBN { get; set; }
+
+	}
+}
